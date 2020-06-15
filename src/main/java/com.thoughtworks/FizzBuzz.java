abd71f9d17cf6d8cf85afa44f52c58a6fce7b676
zhangjing <juslly@163.com>
2020-06-15 22:40:50 +0800

    refactor processInputDoubleNum

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -3,36 +3,39 @@ package com.thoughtworks;
 public class FizzBuzz {
     public String sayResult(int num) {
         String result = "";
-        String Fizz = "Fizz";
-        String Buzz = "Buzz";
-        String Whizz = "Whizz";
         //包含特殊数字处理
         result = processInputContainsSpecialNum(num);
-        //对倍速进行处理
         if (result.equals("")) {
-
-            if (num % 3 == 0) {
-                result = result + Fizz;
-
-            }
-            if (num % 5 == 0) {
-                result = result + Buzz;
-
-            }
-
-            if (num % 7 == 0) {
-                result = result + Whizz;
-            }
-
+            //对倍数进行处理
+            result = processInputDoubleNum(num);
             if ("".equals(result)) {
                 System.out.println(String.valueOf(num));
                 result = String.valueOf(num);
             }
 
         }
-
-
         return result;
+    }
+
+    private String processInputDoubleNum(int num) {
+        String result = "";
+        String Fizz = "Fizz";
+        String Buzz = "Buzz";
+        String Whizz = "Whizz";
+        if (num % 3 == 0) {
+            result = result + Fizz;
+
+        }
+        if (num % 5 == 0) {
+            result = result + Buzz;
+
+        }
+
+        if (num % 7 == 0) {
+            result = result + Whizz;
+        }
+        return result;
+
     }
 
     private String processInputContainsSpecialNum(int num) {
