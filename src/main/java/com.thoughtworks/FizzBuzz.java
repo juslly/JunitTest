d674de416c86fad29939ba010f20efd0b69db77d
zhangjing <juslly@163.com>
2020-06-15 22:35:32 +0800

    refactor processInputContainsSpecialNum

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -1,50 +1,59 @@
 package com.thoughtworks;
 
 public class FizzBuzz {
-    public String sayResult(int num){
-        String result  = "";
+    public String sayResult(int num) {
+        String result = "";
         String Fizz = "Fizz";
         String Buzz = "Buzz";
         String Whizz = "Whizz";
+        //包含特殊数字处理
+        result = processInputContainsSpecialNum(num);
+        //对倍速进行处理
+        if (result.equals("")) {
 
-       // processInputContainsSpecialNum(num);
-        if(String.valueOf(num).contains("7")){
-            System.out.println("resultNum:" + "Fizz");
-            return "Fizz";
+            if (num % 3 == 0) {
+                result = result + Fizz;
 
-        }
+            }
+            if (num % 5 == 0) {
+                result = result + Buzz;
 
-        if(String.valueOf(num).contains("5")){
-            System.out.println("resultNum:" + "Fizz");
-            return "BuzzWhizz";
+            }
 
-        }
-        if(String.valueOf(num).contains("3")){
-            System.out.println("resultNum:" + "Fizz");
-            return "Fizz";
+            if (num % 7 == 0) {
+                result = result + Whizz;
+            }
 
-        }
-
-        if(num % 3 == 0){
-            result  = result + Fizz;
-
-        }
-        if(num % 5 == 0){
-            result  = result + Buzz;
+            if ("".equals(result)) {
+                System.out.println(String.valueOf(num));
+                result = String.valueOf(num);
+            }
 
         }
 
-        if(num % 7 == 0){
-            result  = result + Whizz;
-        }
-
-        if("".equals(result)){
-            System.out.println(String.valueOf(num));
-            result  = String.valueOf(num);
-        }
 
         return result;
     }
 
+    private String processInputContainsSpecialNum(int num) {
 
+        if (String.valueOf(num).contains("7")) {
+            System.out.println("resultNum:" + "Fizz");
+            return "Fizz";
+
+        }
+
+        if (String.valueOf(num).contains("5")) {
+            System.out.println("resultNum:" + "Fizz");
+            return "BuzzWhizz";
+
+        }
+        if (String.valueOf(num).contains("3")) {
+            System.out.println("resultNum:" + "Fizz");
+            return "Fizz";
+
+        }
+        return "";
+
+    }
 }
