0e4d39367993d8ca0ff69d04add8656049f2c2e2
zhangjing <juslly@163.com>
2020-06-15 15:12:39 +0800

    finish all test

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -2,11 +2,44 @@ package com.thoughtworks;
 
 public class FizzBuzz {
     public String sayResult(int num){
+        if(num % 3 == 0 && num % 5 == 0 && num % 7 == 0){
+            System.out.println("resultNum:" + "FizzBuzzWhizz");
+            return "FizzBuzzWhizz";
+
+        }
+        if(num % 3 == 0 && num % 5 == 0 ){
+            System.out.println("resultNum:" + "FizzBuzz");
+            return "FizzBuzz";
+
+        }
+
+        if(num % 3 == 0 && num % 7 == 0 ){
+            System.out.println("resultNum:" + "FizzWhizz");
+            return "FizzWhizz";
+
+        }
+        if(num % 5 == 0 && num % 7 == 0 ){
+            System.out.println("resultNum:" + "BuzzWhizz");
+            return "BuzzWhizz";
+
+        }
+
         if(num % 3 == 0){
             System.out.println("resultNum:" + "Fizz");
             return "Fizz";
 
         }
+        if(num % 5 == 0){
+            System.out.println("resultNum:" + "Buzz");
+            return "Buzz";
+
+        }
+
+        if(num % 7 == 0){
+            System.out.println("resultNum:" + "Whizz");
+            return "Whizz";
+        }
+
         System.out.println("resultNum:" + num);
         return String.valueOf(num);
     }
--- src/test/java/com/thoughtworks/Given1_when_excess_then1.java
+++ src/test/java/com/thoughtworks/Given1_when_excess_then1.java
@@ -2,16 +2,51 @@ package com.thoughtworks;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class Given1_when_excess_then1 {
     FizzBuzz fizzBuzz = new FizzBuzz();
     @Test
     public void Given1_when_excess_then1_test(){
         fizzBuzz.sayResult(1);
+        assertEquals("1","1");
     }
 
     @Test
     public void Given3_when_excess_then3_test(){
         fizzBuzz.sayResult(3);
+        assertEquals("Fizz","Fizz");
+    }
+
+    @Test
+    public void Given5_when_excess_then5_test(){
+        fizzBuzz.sayResult(5);
+        assertEquals("Buzz","Buzz");
+
+    }
+
+    @Test
+    public void Given15_when_excess_then15_test(){
+        fizzBuzz.sayResult(15);
+        assertEquals("FizzBuzz","FizzBuzz");
+    }
+
+    @Test
+    public void Given7_when_excess_then7_test(){
+        fizzBuzz.sayResult(7);
+        assertEquals("Whizz","Whizz");
+    }
+
+    @Test
+    public void Given21_when_excess_then21_test(){
+        fizzBuzz.sayResult(21);
+        assertEquals("FizzWhizz","FizzWhizz");
+    }
+
+    @Test
+    public void Given35_when_excess_then35_test(){
+        fizzBuzz.sayResult(35);
+        assertEquals("BuzzWhizz","BuzzWhizz");
     }
 
 
