468240ff6a0f75475be27814a38315d2d809af8c
zhangjing <juslly@163.com>
2020-06-15 15:22:00 +0800

    refixed five test

--- src/test/java/com/thoughtworks/Given1_when_excess_then1.java
+++ src/test/java/com/thoughtworks/Given1_when_excess_then1.java
@@ -7,46 +7,46 @@ import static org.junit.Assert.assertEquals;
 public class Given1_when_excess_then1 {
     FizzBuzz fizzBuzz = new FizzBuzz();
     @Test
-    public void Given1_when_excess_then1_test(){
-        fizzBuzz.sayResult(1);
-        assertEquals("1","1");
+    public void given1_when_excess_then1_test(){
+       String str =  fizzBuzz.sayResult(1);
+        assertEquals("1",str);
     }
 
     @Test
-    public void Given3_when_excess_then3_test(){
-        fizzBuzz.sayResult(3);
-        assertEquals("Fizz","Fizz");
+    public void given3_when_excess_then3_test(){
+        String str = fizzBuzz.sayResult(3);
+        assertEquals("Fizz",str);
     }
 
     @Test
-    public void Given5_when_excess_then5_test(){
-        fizzBuzz.sayResult(5);
-        assertEquals("Buzz","Buzz");
+    public void given5_when_excess_then5_test(){
+        String str = fizzBuzz.sayResult(5);
+        assertEquals("Buzz",str);
 
     }
 
     @Test
-    public void Given15_when_excess_then15_test(){
-        fizzBuzz.sayResult(15);
-        assertEquals("FizzBuzz","FizzBuzz");
+    public void given15_when_excess_then15_test(){
+        String str = fizzBuzz.sayResult(15);
+        assertEquals("FizzBuzz",str);
     }
 
     @Test
-    public void Given7_when_excess_then7_test(){
-        fizzBuzz.sayResult(7);
-        assertEquals("Whizz","Whizz");
+    public void given7_when_excess_then7_test(){
+        String str = fizzBuzz.sayResult(7);
+        assertEquals("Whizz",str);
     }
 
     @Test
-    public void Given21_when_excess_then21_test(){
-        fizzBuzz.sayResult(21);
-        assertEquals("FizzWhizz","FizzWhizz");
+    public void given21_when_excess_then21_test(){
+        String str = fizzBuzz.sayResult(21);
+        assertEquals("FizzWhizz",str);
     }
 
     @Test
-    public void Given35_when_excess_then35_test(){
-        fizzBuzz.sayResult(35);
-        assertEquals("BuzzWhizz","BuzzWhizz");
+    public void given35_when_excess_then35_test(){
+        String str = fizzBuzz.sayResult(35);
+        assertEquals("BuzzWhizz",str);
     }
 
 
